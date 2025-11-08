ed6375a1ba4c50bb57bef436ea8aacf9911a3b50
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-09 00:42:09 +0500

    Update ShapeVisitor.java

--- src/ShapeVisitor.java
+++ src/ShapeVisitor.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class ShapeDrawer implements ShapeVisitor {
+    @Override
+    public void visit(Circle circle) {
+        System.out.println("Drawing a circle with radius " + circle.getRadius());
+    }
 
-public class ShapeVisitor {
+    @Override
+    public void visit(Rectangle rectangle) {
+        System.out.println("Drawing a rectangle with width "
+                + rectangle.getWidth() + " and height " + rectangle.getHeight());
+    }
 }
