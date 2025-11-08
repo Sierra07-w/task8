d31010af503fb36e58fadebd1633d5a5c51898df
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-09 00:39:59 +0500

    Update AreaCalculator.java

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class AreaCalculator implements ShapeVisitor {
+    @Override
+    public void visit(Circle circle) {
+        double area = Math.PI * Math.pow(circle.getRadius(), 2);
+        System.out.println("Circle area: " + area);
+    }
 
-public class AreaCalculator {
+    @Override
+    public void visit(Rectangle rectangle) {
+        double area = rectangle.getWidth() * rectangle.getHeight();
+        System.out.println("Rectangle area: " + area);
+    }
 }
