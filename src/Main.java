e66bc1e988c34f88858e051c42d6b09c8b78e19a
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-09 00:40:44 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ShapeVisitor areaCalculator = new AreaCalculator();
+        ShapeVisitor drawer = new ShapeDrawer();
+
+
+        circle.accept(areaCalculator);
+        rectangle.accept(areaCalculator);
+
+
+        circle.accept(drawer);
+        rectangle.accept(drawer);
     }
 }
