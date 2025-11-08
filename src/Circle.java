911624a986bf93704d1745e26dea913e6c00637e
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-09 00:40:19 +0500

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Circle implements Shape {
+    private final double radius;
 
-public class Circle {
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void accept(ShapeVisitor visitor) {
+        visitor.visit(this);
+    }
 }
