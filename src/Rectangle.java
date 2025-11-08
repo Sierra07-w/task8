30964048f65e6f541433de1c7af24555af0361ab
Sierra07-w <mukhammadsara07@gmail.com>
2025-11-09 00:41:06 +0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Rectangle implements Shape {
+    private final double width;
+    private final double height;
 
-public class Rectangle {
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void accept(ShapeVisitor visitor) {
+        visitor.visit(this);
+    }
 }
